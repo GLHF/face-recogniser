bf9b9cc481f3ab193886c2c9a0f3b93965ec3076
Timm <timur.galiakberov@simbirsoft.com>
2020-12-25 04:54:54 +0300

    РАБОТАЕТ БЛЭЭЭЭТ

--- src/main/java/ru/kpfu/itis/timm/facerecogniser/controller/ImageController.java
+++ src/main/java/ru/kpfu/itis/timm/facerecogniser/controller/ImageController.java
@@ -1,24 +1,23 @@
 package ru.kpfu.itis.timm.facerecogniser.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 @Controller
-@RequestMapping("/image")
 @Slf4j
 @RequiredArgsConstructor
 public class ImageController {
 
-    @PostMapping("/recognise")
-    public ResponseEntity recognise() throws IOException {
-        return ResponseEntity.ok("Hello");
+    @PostMapping("/image")
+    public ResponseEntity recognise() throws Exception{
+        return ResponseEntity.ok(new ObjectMapper().writeValueAsString(Arrays.asList("Идите", "В", "ЖОПУ")));
     }
 }
